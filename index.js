d592a26cf63bfbe322479fa3c8471019991f9e19
Sujan <sujanhebbarhs@gmail.com>
2023-05-29 10:03:13 +0530

    Changed index.js by sujan

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ logger.on('message',(data) => console.log('Called Listener:',data));
 //logger.on is used to listen to the event called message and then we pass a callback function which will be called when the event is emitted and the data is passed to the callback function as a parameter which we can use in the callback function
 
 logger.log('Hello World!'); // we are calling the log method of the Logger class which is defined in the logger.js file and passing the message as a parameter to the log method which will emit the message event and the callback function will be called and the message will be printed on the console
+
+//Adding new shit heheh
+//Adding new shit heheh
+//Adding new shit heheh
+//Adding new shit heheh
+//Adding new shit heheh
+
+//by doffy
